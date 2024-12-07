1fd2cfd912c278ed09d090551585eeb45cb19ef6
malloc37 <kadrii.dev@gmail.com>
2024-12-07 15:39:25 +0100

    fix options_list.py

--- components/options_list.py
+++ components/options_list.py
@@ -6,39 +6,43 @@ investment_options = {
         {"Instrument": "Vanguard Total Bond Market ETF", "Ticker": "BND", "Asset Class": "Bonds"},
         {"Instrument": "Invesco Ultra Short Duration ETF", "Ticker": "GSY", "Asset Class": "Bonds"},
         {"Instrument": "Schwab U.S. Aggregate Bond ETF", "Ticker": "SCHZ", "Asset Class": "Bonds"},
-        {"Instrument": "U.S. Treasury 10-Year Note Proxy", "Ticker": "TNX", "Asset Class": "Bonds"}
+        {"Instrument": "U.S. Treasury 10-Year Note Proxy", "Ticker": "TNX", "Asset Class": "Bonds"},
+        {"Instrument": "iShares Short Treasury Bond ETF", "Ticker": "SHV", "Asset Class": "ETF"},
     ],
     "Low risk": [
-        {"Instrument": "iShares Short Treasury Bond ETF", "Ticker": "SHV", "Asset Class": "ETF"},
         {"Instrument": "SPDR S&P Dividend ETF", "Ticker": "SDY", "Asset Class": "ETF"},
         {"Instrument": "Vanguard Dividend Appreciation ETF", "Ticker": "VIG", "Asset Class": "ETF"},
-        {"Instrument": "Vanguard High Dividend Yield ETF", "Ticker": "VYM", "Asset Class": "ETF"}
+        {"Instrument": "Vanguard High Dividend Yield ETF", "Ticker": "VYM", "Asset Class": "ETF"},
+        {"Instrument": "Realty Income Corporation", "Ticker": "O", "Asset Class": "REIT"},
+        {"Instrument": "Johnson & Johnson", "Ticker": "JNJ", "Asset Class": "Stock"},
+        {"Instrument": "Coca-Cola Company", "Ticker": "KO", "Asset Class": "Stock"},
+        {"Instrument": "Procter & Gamble", "Ticker": "PG", "Asset Class": "Stock"},
+
     ],
     "Moderate risk": [
         {"Instrument": "Tesla Inc.", "Ticker": "TSLA", "Asset Class": "Stock"},
         {"Instrument": "Apple Inc.", "Ticker": "AAPL", "Asset Class": "Stock"},
         {"Instrument": "ARK Innovation ETF", "Ticker": "ARKK", "Asset Class": "ETF"},
-        {"Instrument": "SPDR Dow Jones Industrial Average ETF", "Ticker": "DIA", "Asset Class": "ETF"}
+        {"Instrument": "SPDR Dow Jones Industrial Average ETF", "Ticker": "DIA", "Asset Class": "ETF"},
+        {"Instrument": "SPDR S&P 500 ETF", "Ticker": "SPY", "Asset Class": "ETF"},
+        {"Instrument": "Vanguard Growth ETF", "Ticker": "VUG", "Asset Class": "ETF"},
+        {"Instrument": "Ethereum", "Ticker": "ETH-USD", "Asset Class": "Crypto"},
     ],
     "High risk": [
-        {"Instrument": "Vanguard Growth ETF", "Ticker": "VUG", "Asset Class": "ETF"},
         {"Instrument": "Invesco QQQ Trust", "Ticker": "QQQ", "Asset Class": "ETF"},
         {"Instrument": "Vanguard Information Technology ETF", "Ticker": "VGT", "Asset Class": "ETF"},
         {"Instrument": "ARK Fintech Innovation ETF", "Ticker": "ARKF", "Asset Class": "ETF"},
-        {"Instrument": "Vanguard Emerging Markets ETF", "Ticker": "VWO", "Asset Class": "ETF"}
+        {"Instrument": "Vanguard Emerging Markets ETF", "Ticker": "VWO", "Asset Class": "ETF"},
+        {"Instrument": "NVIDIA Corporation", "Ticker": "NVDA", "Asset Class": "Stock"},
+        {"Instrument": "Shopify", "Ticker": "SHOP", "Asset Class": "Stock"},
+        {"Instrument": "Square Inc.", "Ticker": "SQ", "Asset Class": "Stock"},
+
     ],
     "Very-high risk": [
         {"Instrument": "Bitcoin", "Ticker": "BTC-USD", "Asset Class": "Crypto"},
         {"Instrument": "Ethereum", "Ticker": "ETH-USD", "Asset Class": "Crypto"},
         {"Instrument": "Dogecoin", "Ticker": "DOGE-USD", "Asset Class": "Crypto"},
         {"Instrument": "Solana", "Ticker": "SOL-USD", "Asset Class": "Crypto"},
-        {"Instrument": "Realty Income Corporation", "Ticker": "O", "Asset Class": "REIT"},
-        {"Instrument": "Johnson & Johnson", "Ticker": "JNJ", "Asset Class": "Stock"},
-        {"Instrument": "Coca-Cola Company", "Ticker": "KO", "Asset Class": "Stock"},
-        {"Instrument": "Procter & Gamble", "Ticker": "PG", "Asset Class": "Stock"},
-        {"Instrument": "NVIDIA Corporation", "Ticker": "NVDA", "Asset Class": "Stock"},
-        {"Instrument": "Shopify", "Ticker": "SHOP", "Asset Class": "Stock"},
-        {"Instrument": "Square Inc.", "Ticker": "SQ", "Asset Class": "Stock"},
         {"Instrument": "AMC Entertainment", "Ticker": "AMC", "Asset Class": "Stock"},
         {"Instrument": "GameStop Corporation", "Ticker": "GME", "Asset Class": "Stock"},
         {"Instrument": "MicroStrategy Incorporated", "Ticker": "MSTR", "Asset Class": "Stock"}
