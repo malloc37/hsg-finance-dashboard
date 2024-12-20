02c0e8457be18f5b9a72434c98bf6a82100c090c
NGoumaz <nico.goumaz@gmail.com>
2024-12-20 16:38:06 +0100

    Update options_list.py

--- components/options_list.py
+++ components/options_list.py
@@ -1,4 +1,53 @@
+investment_options = {
+    "Very-low risk": [
+        {"Instrument": "Invesco Ultra Short Duration ETF", "Ticker": "GSY", "Asset Class": "Bonds"},
+        {"Instrument": "iShares Core U.S. Aggregate Bond ETF", "Ticker": "AGG", "Asset Class": "Bonds"},
+        {"Instrument": "iShares 0-3 Month Treasury Bond ETF", "Ticker": "SGOV", "Asset Class": "ETF"},
+        {"Instrument": "Schwab U.S. Aggregate Bond ETF", "Ticker": "SCHZ", "Asset Class": "Bonds"},
+        {"Instrument": "SPDR Bloomberg 1-3 Month T-Bill ETF", "Ticker": "BIL", "Asset Class": "Bonds"},
+        {"Instrument": "iShares Short Treasury Bond ETF", "Ticker": "SHV", "Asset Class": "ETF"},
+        {"Instrument": "Vanguard Total Bond Market ETF", "Ticker": "BND", "Asset Class": "Bonds"}
+    ],
+    "Low risk": [
+        {"Instrument": "Nestlé", "Ticker": "NESN.SW", "Asset Class": "Stock"},
+        {"Instrument": "Procter & Gamble", "Ticker": "PG", "Asset Class": "Stock"},
+        {"Instrument": "SPDR S&P Dividend ETF", "Ticker": "SDY", "Asset Class": "ETF"},
+        {"Instrument": "Vanguard Dividend Appreciation ETF", "Ticker": "VIG", "Asset Class": "ETF"},
+        {"Instrument": "Vanguard High Dividend Yield ETF", "Ticker": "VYM", "Asset Class": "ETF"},
+        {"Instrument": "Realty Income Corporation", "Ticker": "O", "Asset Class": "REIT"},
+        {"Instrument": "Johnson & Johnson", "Ticker": "JNJ", "Asset Class": "Stock"},
+        {"Instrument": "Coca-Cola Company", "Ticker": "KO", "Asset Class": "Stock"},
+        {"Instrument": "Procter & Gamble", "Ticker": "PG", "Asset Class": "Stock"},
 
+    ],
+    "Moderate risk": [
+        {"Instrument": "Apple Inc.", "Ticker": "AAPL", "Asset Class": "Stock"},
+        {"Instrument": "ARK Innovation ETF", "Ticker": "ARKK", "Asset Class": "ETF"},
+        {"Instrument": "Hermès", "Ticker": "RMS.PA", "Asset Class": "Stock"},
+        {"Instrument": "SPDR Dow Jones Industrial Average ETF", "Ticker": "DIA", "Asset Class": "ETF"},
+        {"Instrument": "SPDR S&P 500 ETF", "Ticker": "SPY", "Asset Class": "ETF"},
+        {"Instrument": "Tesla Inc.", "Ticker": "TSLA", "Asset Class": "Stock"},
+        {"Instrument": "Vanguard Growth ETF", "Ticker": "VUG", "Asset Class": "ETF"}
+    ],
+    "High risk": [
+        {"Instrument": "Bitcoin", "Ticker": "BTC-USD", "Asset Class": "Crypto"},
+        {"Instrument": "Ethereum", "Ticker": "ETH-USD", "Asset Class": "Crypto"},
+        {"Instrument": "Invesco QQQ Trust", "Ticker": "QQQ", "Asset Class": "ETF"},
+        {"Instrument": "NVIDIA Corporation", "Ticker": "NVDA", "Asset Class": "Stock"},
+        {"Instrument": "Shopify", "Ticker": "SHOP", "Asset Class": "Stock"},
+        {"Instrument": "Square Inc.", "Ticker": "SQ", "Asset Class": "Stock"},
+        {"Instrument": "Vanguard Information Technology ETF", "Ticker": "VGT", "Asset Class": "ETF"}
+    ],
+    "Very-high risk": [
+        {"Instrument": "VanEck Semiconductor ETF", "Ticker": "SMH", "Asset Class": "ETF"},
+        {"Instrument": "ARK Fintech Innovation ETF", "Ticker": "ARKF", "Asset Class": "ETF"},
+        {"Instrument": "Dogecoin", "Ticker": "DOGE-USD", "Asset Class": "Crypto"},
+        {"Instrument": "MicroStrategy Incorporated", "Ticker": "MSTR", "Asset Class": "Stock"},
+        {"Instrument": "Riot Platforms Inc.", "Ticker": "RIOT", "Asset Class": "Stock"},
+        {"Instrument": "Solana", "Ticker": "SOL-USD", "Asset Class": "Crypto"},
+        {"Instrument": "Vanguard Emerging Markets ETF", "Ticker": "VWO", "Asset Class": "ETF"}
+    ]
+}
 
 
 
